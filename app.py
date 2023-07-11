c1ef244fc1c2327c633261d9e3c11c8d34db0266
muhamadsahrulll <sahrulsmp4@gmail.com>
2023-07-11 19:53:17 +0700

    update big

--- app.py
+++ app.py
@@ -1,20 +1,35 @@
-import streamlit as st
 import os
+import streamlit as st
+import numpy as np
+import pandas as pd
+from sqlalchemy import create_engine
+# import pyodbc
 
-
-st.write("""# My first app""")
-st.write("DB username:", st.secrets["db_username"])
-st.write("DB password:", st.secrets["db_password"])
-st.write("My cool secrets:", st.secrets["1234"])
-# Inisialisasi koneksi.
-# streamlit_app.py
-st.write(
-    "Has environment variables been set:",
-    os.environ["username"] == st.secrets["username"],
+engine = create_engine(
+    # "mysql+pymysql://<username>:<password>@<host>/<dbname>[?<options>]"
+    "mysql+mysqldb://root:qTi5zejHl22POl6ZkUTa@containers-us-west-64.railway.app:7086/railway", 
 )
-# Initialize connection.
-conn = st.experimental_connection('mysql', type='sql')
 
-# Perform query.
-df = conn.query('SELECT * from users;', ttl=600)
+# st.write("""# My first app""")
+# st.write("DB username:", st.secrets["db_username"])
+# st.write("DB password:", st.secrets["db_password"])
+# # st.write("My cool secrets:", st.secrets["1234"])
+# # Inisialisasi koneksi.
+# # streamlit_app.py
+# # st.write(
+# #     "Has environment variables been set:",
+# #     os.environ["username"] == st.secrets["username"],
+# # )
+# # Initialize connection.
+# conn = st.experimental_connection('mysql', type='sql')
+
+# # Perform query.
+df = pd.read_sql('SELECT * from users;', engine)
+# print(df)
+# df = engine.query('SELECT * from users;', ttl=600)
+# import streamlit as st
+# import numpy as np
+
+# dataframe = np.random.randn(10, 20)
+st.dataframe(df)
 
