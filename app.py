ac30f34acc9dcfb88623f5769d15cd091612151f
muhamadsahrulll <sahrulsmp4@gmail.com>
2023-07-11 18:00:53 +0700

    pertama

---
+++ app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import pandas as pd 
+import numpy as np
+import pymysql
+
+
+
+st.write("""# My first app""")
+
+# Inisialisasi koneksi.
+conn = st.experimental_connection('mysql', type='sql', dialect='mysql', host='containers-us-west-64.railway.app', username='root', password='qTi5zejHl22POl6ZkUTa', database='railway')
+
+# Melakukan query.
+df = conn.query('SELECT * from users;', ttl=0)
