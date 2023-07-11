e4d6d74c2b22986a0ca9af2c3af5e9f96eb44b05
muhamadsahrulll <sahrulsmp4@gmail.com>
2023-07-11 18:11:32 +0700

    3

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 import streamlit as st
-import pandas as pd 
-import numpy as np
 
 
 
 st.write("""# My first app""")
 
 # Inisialisasi koneksi.
-conn = st.experimental_connection('mysql', type='sql', dialect='mysql', host='containers-us-west-64.railway.app', username='root', password='qTi5zejHl22POl6ZkUTa', database='railway')
+# streamlit_app.py
 
-# Melakukan query.
-df = conn.query('SELECT * from users;', ttl=0)
+# Initialize connection.
+conn = st.experimental_connection('mysql', type='sql')
+
+# Perform query.
+df = conn.query('SELECT * from users;', ttl=600)
+
