a619fbf0d2edffdce80548608deb6850a4ff45d0
muhamadsahrulll <sahrulsmp4@gmail.com>
2023-07-11 19:15:12 +0700

    halah

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 import streamlit as st
-
+import os
 
 
 st.write("""# My first app""")
-
+st.write("DB username:", st.secrets["db_username"])
+st.write("DB password:", st.secrets["db_password"])
+st.write("My cool secrets:", st.secrets["1234"])
 # Inisialisasi koneksi.
 # streamlit_app.py
-
+st.write(
+    "Has environment variables been set:",
+    os.environ["username"] == st.secrets["username"],
+)
 # Initialize connection.
 conn = st.experimental_connection('mysql', type='sql')
 
